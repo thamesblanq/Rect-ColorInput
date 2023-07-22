11879438071fbce3f0f47244e59f4c414516b3a0
blanq31 <obiwulugodswill@gmail.com>
2023-07-22 16:42:44 +0100

    woah

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className='header-text'>
+        React Project
+      </h1>
+     </header>
+  )
+}
+
+export default Header
